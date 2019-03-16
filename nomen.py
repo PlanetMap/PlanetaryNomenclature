7141abf8f1d3f184f010d38b1e5d7d9708b754b4
Anderson Moyers <amoyers@tomato>
2019-03-15 17:22:21 -0700

    basic view layout

--- nomen.py
+++ nomen.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return 'Planetary Nomenclature'
+def index_handler():
+	return render_template('page_template.html')
 
-
-app.run(host = '0.0.0.0', port = 5000, debug=True)
+if __name__ == "__main__":
+	app.run(host = '0.0.0.0', port = 5000, debug=True)
